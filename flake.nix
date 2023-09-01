9f3bbaaef6449f99bc00899e9a63520efac0ec7f
ketsumashu <k2mashu@icloud.com>
2023-09-01 18:55:13 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs; {
+  outputs = inputs: {
     nixosConfigurations = {
       myNixOS = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
