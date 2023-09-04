5d37c90b2c7766cad5d09bce6a601bed73cc4dc8
ketsumashu <k2mashu@icloud.com>
2023-09-04 11:49:39 +0900

    2

--- home/programs/gui/terminals/wezterm.nix
+++ home/programs/gui/terminals/wezterm.nix
@@ -2,8 +2,4 @@
   programs.wezterm = {
     enable = true;
   };
-  home.file = {
-    ".config/wezterm/wezterm.lua".source = ./wezterm.lua;
-    ".config/wezterm/colors/substrata.toml".source = ./colors/substrata.toml;
-  };
 }
